801744f9ba9e830936dc447510d26abf117c1789
Robin Southern <betajaen@ihoed.com>
2017-08-12 19:55:00 +0100

    Copyright + Basic typedefs

--- synthwave.c
+++ synthwave.c
@@ -1,1 +1,8 @@
+//  synthwave.h
+//  
+//  Copyright (C) 2017 Robin Southern   https://github.com/betajaen/synthwave
+//  
+//  This software may be modified and distributed under the terms
+//  of the MIT license.  See the LICENSE file for details.
+
 #include "synthwave.h"
--- synthwave.h
+++ synthwave.h
@@ -1,4 +1,25 @@
+//  synthwave.h
+//  
+//  Copyright (C) 2017 Robin Southern   https://github.com/betajaen/synthwave
+//  
+//  This software may be modified and distributed under the terms
+//  of the MIT license.  See the LICENSE file for details.
+
 #ifndef SYNTHWAVE_H
 #define SYNTHWAVE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+typedef float    f32;
+typedef double   f64;
 
 #endif
